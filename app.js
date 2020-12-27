c7e51ce0e54d617f54515deeb0408ea3a8d80d56
marc paul parker <marcpaul.parker@gmail.com>
2020-12-27 13:35:28 -0500

    post working

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
+const jsonParser = bodyParser.json()
 const port = 59595
-var repertoire= '*'
+const repertoire= '/'
+
 var index = 0
 
 var msg = 'Hello World';
 console.log(msg);
 
+app.use(jsonParser)
+
 app.get(repertoire, (req, res) => {
     console.log(req, index++)
     res.send(`Hello World! ${index - 1}`)
 })
-  
+
+app.post(repertoire, (req, res) => {
+    console.log(req.body, index++)
+    res.send(`Hello World! ${index - 1}`)
+})
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
